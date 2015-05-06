afcfd281e393156775a98e961e8a263218a71e16
DK <daeyk3@uci.edu>
2015-05-05 23:27:45 -0700

    Death and moving added

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -9,6 +9,15 @@ public class Character : MonoBehaviour {
 	public int bombPower;
 	public float speed;
 	public bool kickItem;
+	private Animator animator;
+	public int deathCount;
+	public bool dying;
+	void Awake()
+	{
+		animator = GetComponent<Animator> ();
+		deathCount = 0;
+		dying = false;
+	}
 
 
 	// Use this for initialization
@@ -19,16 +28,30 @@ public class Character : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		//when the player dies, give them few frames of break with death animation
+		
+		if (deathCount > 1) {
+			deathCount--;
+		} else if(deathCount==1){
+			this.transform.position = new Vector2 (1, 1);
+			deathCount=0;
+			dying=false;
+		}
+
 	}
 
 
 	void OnTriggerEnter2D(Collider2D col)
 	{
-		if (col.tag == "Explosion") 
+		//when the player dies
+		if (col.tag == "Explosion" && !dying) 
 		{
 			life--;
-			this.transform.position = new Vector2(1,1);
+			deathCount=90;
+			animator.SetTrigger ("Death");
+			dying=true;
 		}
 	}
+
+
 }
--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -5,6 +5,9 @@ public class Controls : MonoBehaviour {
 	public float runSpeed = 8f;
 	public GameObject bomb;
 
+	private Animator animator;
+	public Character _Character;
+
 	private Character bomberman;
 	private Rigidbody2D rigidBody;
 	// Use this for initialization
@@ -12,55 +15,76 @@ public class Controls : MonoBehaviour {
 		//bomb = Resources.Load ("asset/Bomb");
 		bomberman = this.transform.GetComponent<Character> ();
 		rigidBody = this.GetComponent<Rigidbody2D>();
+
+		_Character = GetComponent<Character> ();
+
+	}
+	void Awake()
+	{
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-	
-		if( Input.GetKey( KeyCode.UpArrow ) )
-		{
-			//transform.Translate(new Vector2(0,runSpeed*Time.deltaTime));
-			rigidBody.MovePosition((Vector2)this.transform.position + new Vector2(0,1) * runSpeed*Time.deltaTime);
-		}
-		if( Input.GetKey( KeyCode.DownArrow ) )
-		{
-			//transform.Translate(new Vector2(0,-runSpeed*Time.deltaTime));
-			rigidBody.MovePosition((Vector2)this.transform.position + new Vector2(0,-1) * runSpeed*Time.deltaTime);
-		}
-		if( Input.GetKey( KeyCode.LeftArrow ) )
-		{
-			//transform.Translate(new Vector2(-runSpeed*Time.deltaTime,0));
-			rigidBody.MovePosition((Vector2)this.transform.position + new Vector2(-1,0) * runSpeed*Time.deltaTime);
-		}
-		if( Input.GetKey( KeyCode.RightArrow ) )
-		{
-			//transform.Translate(new Vector2(runSpeed*Time.deltaTime,0));
-			rigidBody.MovePosition((Vector2)this.transform.position + new Vector2(1,0) * runSpeed*Time.deltaTime);
-		}
+		//character can not move during dying animation
+	if (!_Character.dying) {
+			if (Input.GetKeyDown (KeyCode.UpArrow)) {
+				animator.SetTrigger ("Up");
+			}
+			if (Input.GetKeyDown (KeyCode.DownArrow)) {
+				animator.SetTrigger ("Down");
+			}
+			if (Input.GetKeyDown (KeyCode.RightArrow)) {
+				animator.SetTrigger ("Right");
+			}
+			if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+				animator.SetTrigger ("Left");
+			}
 
-		if( Input.GetKeyDown( KeyCode.Space ) )
-		{
-			float x = Mathf.Round (this.transform.position.x);
-			float y = Mathf.Round (this.transform.position.y);
-			Collider2D[] colObjs = Physics2D.OverlapCircleAll (new Vector2(x,y), 0.3f);
-			bool bombPlaced = false;
+
+
+
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				//transform.Translate(new Vector2(0,runSpeed*Time.deltaTime));
+				rigidBody.MovePosition ((Vector2)this.transform.position + new Vector2 (0, 1) * runSpeed * Time.deltaTime);
+
+
+			}
+			if (Input.GetKey (KeyCode.DownArrow)) {
+				//transform.Translate(new Vector2(0,-runSpeed*Time.deltaTime));
+				rigidBody.MovePosition ((Vector2)this.transform.position + new Vector2 (0, -1) * runSpeed * Time.deltaTime);
+				animator.SetInteger ("Direction", 3);
+			}
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+				//transform.Translate(new Vector2(-runSpeed*Time.deltaTime,0));
+				rigidBody.MovePosition ((Vector2)this.transform.position + new Vector2 (-1, 0) * runSpeed * Time.deltaTime);
+				animator.SetInteger ("Direction", 4);
+			}
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				//transform.Translate(new Vector2(runSpeed*Time.deltaTime,0));
+				rigidBody.MovePosition ((Vector2)this.transform.position + new Vector2 (1, 0) * runSpeed * Time.deltaTime);
+
+			}
+
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				float x = Mathf.Round (this.transform.position.x);
+				float y = Mathf.Round (this.transform.position.y);
+				Collider2D[] colObjs = Physics2D.OverlapCircleAll (new Vector2 (x, y), 0.3f);
+				bool bombPlaced = false;
 			
-			foreach(Collider2D col in colObjs)
-			{
-				if(col.tag == "Bomb")
-				{
-					bombPlaced = true;
+				foreach (Collider2D col in colObjs) {
+					if (col.tag == "Bomb") {
+						bombPlaced = true;
+					}
 				}
-			}
 			
-			if(bomberman.liveBombs < bomberman.bombLimit && !bombPlaced)
-			{
-				DropBomb();
-				bomberman.liveBombs++;
+				if (bomberman.liveBombs < bomberman.bombLimit && !bombPlaced) {
+					DropBomb ();
+					bomberman.liveBombs++;
+				}
+
 			}
-
 		}
-
 
 	}
 
